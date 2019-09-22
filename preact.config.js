5ba57fe12e7ae2ab5157b096019b34e41905974e
Tobias Zucali <tobias@zucali.at>
2019-09-22 09:49:34 +0200

    resources in dev mode (part 2b)

--- preact.config.js
+++ preact.config.js
@@ -1,14 +1,11 @@
 /* eslint-disable no-param-reassign */
+import devData from './devData.json'
 
 export default function webpack(config, env, helpers, options) {
-  /** you can change the config here * */
-
-  // throw JSON.stringify(
-  //   helpers.getPluginsByName(config, 'HtmlWebpackPlugin'),
-  //   null,
-  //   2
-  // )
-  if (env.isProd) {
+  if (process.env.NODE_ENV === 'production') {
     config.output.publicPath = '/assets/templates/zucali-preact-modx-template_build/'
+  } else {
+    const htmlWebpackPlugin = helpers.getPluginsByName(config, 'HtmlWebpackPlugin')[0]
+    htmlWebpackPlugin.plugin.options.devData = JSON.stringify(devData, null, 2)
   }
 }
--- src/hooks/useResources/devResources.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-mutable-exports, global-require */
-
-let data
-if (process.env.NODE_ENV === 'development') {
-  data = require('../../../devData.json')
-}
-
-export default data
